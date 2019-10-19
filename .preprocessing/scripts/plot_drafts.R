0cf0c28a08a2b97d52e6da2e9f3f2ee8c28ef6b2
maryzam <zamo.code@gmail.com>
2019-10-19 22:38:48 +0300

    chart drafts

--- .preprocessing/scripts/plot_drafts.R
+++ .preprocessing/scripts/plot_drafts.R
@@ -2,14 +2,35 @@ library(dplyr)
 library(ggplot2)
 
 songs <- left_join(commercial, audio_features_all, by="spotify_id") 
-typeof(songs$danceability)
 
-songs <- songs %>% filter(!is.na(danceability)) %>% filter(year > 2010.0)
+songs <- songs %>% filter(!is.na(danceability)) %>% filter(year > 2005.0)
 
 songs$year <- as.factor(songs$year)
 
-dance_plot <- ggplot(songs, aes(x=year, y=danceability)) +
-            geom_violin(trim = FALSE) +
-            stat_summary(fun.y=median, geom="point", size=1, color="steelblue") +
-            stat_summary(fun.data=mean_sdl,  fun.args = list(mult = 1), geom="pointrange", color="lightblue")
-dance_plot
+draw_feature <- function(dset, group_by, feature) {
+  
+  feature_plot <- ggplot(songs, aes_string(x=group_by, y=feature)) +
+    stat_summary(fun.data=mean_sdl, fun.args = list(mult = 1), geom="pointrange", color="steelblue") +
+    stat_summary(fun.y=median, geom = "smooth",aes(group=1), color="skyblue") +
+    stat_summary(fun.y=median, geom="point", size=2, color="skyblue")
+  
+  feature_plot <- feature_plot + theme_bw() + theme(axis.text.x = element_text(angle = 45, hjust = 1))
+  
+  feature_plot
+  
+  plot_name <- paste0(group_by, '_', feature, '.png')
+  ggsave(plot_name)
+}
+
+library(purrr)
+
+combinations <- list(
+  group_key =  c("year", "company"),
+  feature =  c("danceability", "energy", "liveness", "valence", "tempo", "loudness","mode", "acousticness", "instrumentalness")
+) %>% cross_df() 
+
+combinations %>% 
+  rowwise %>% 
+  do(out=draw_feature(songs, .$group_key, .$feature))
+
+
